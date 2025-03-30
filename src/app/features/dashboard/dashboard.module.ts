efa0fb353f2e6408b0263bf5a6943e81b9af7277
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-30 22:45:34 +0200

    feat(dashboard): add DashboardPageComponent with template and styles

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardPageComponent
   ],
   imports: [
     CommonModule,
