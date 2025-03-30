2fec88f1e520ebc33325b0194eb14a619f731ec0
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-30 22:09:55 +0200

    feat(shared): add ConfigCardComponent to SharedModule with template and styles

---
+++ src/app/shared/components/config-card/config-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-config-card',
+  standalone: false,
+  templateUrl: './config-card.component.html',
+  styleUrl: './config-card.component.scss'
+})
+export class ConfigCardComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConfigCardComponent } from './components/config-card/config-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ConfigCardComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ConfigCardComponent
   ]
 })
 export class SharedModule { }
