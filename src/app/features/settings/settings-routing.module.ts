d90b4af1e961a90b1778efe53876d1be80876db4
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:04:47 +0200

    feat(settings): add routing for settings components in settings module

--- src/app/features/settings/settings-routing.module.ts
+++ src/app/features/settings/settings-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Import components
+import { CommissionSettingsPageComponent } from './commission-settings-page/commission-settings-page.component';
+import { BonusRulesPageComponent } from './bonus-rules-page/bonus-rules-page.component';
+import { DisplaySettingsPageComponent } from './display-settings-page/display-settings-page.component';
+import { OtherConfigsPageComponent } from './other-configs-page/other-configs-page.component';
+
+const routes: Routes = [
+  // Redirect base settings path or show an overview? Redirecting for now.
+  { path: '', redirectTo: 'commission', pathMatch: 'full' },
+  { path: 'commission', component: CommissionSettingsPageComponent },
+  { path: 'bonus-rules', component: BonusRulesPageComponent },
+  { path: 'display', component: DisplaySettingsPageComponent },
+  { path: 'other', component: OtherConfigsPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SettingsRoutingModule { }
+export class SettingsRoutingModule {}
