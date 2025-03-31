eb68e77ff0146cc0b36f56d030181400bad41ea2
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:17:58 +0200

    feat(earnings): refactor EarningsModule to use SharedModule and include Material components

--- src/app/features/earnings/earnings.module.ts
+++ src/app/features/earnings/earnings.module.ts
@@ -1,19 +1,39 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
 
 import { EarningsRoutingModule } from './earnings-routing.module';
-import { EarningsComponent } from './earnings.component';
 import { EarningsPageComponent } from './earnings-page/earnings-page.component';
 
+// Required Material Modules
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip'; // Added Tooltip
+import { MatSnackBarModule } from '@angular/material/snack-bar'; // Added SnackBar
+import { MatDialogModule } from '@angular/material/dialog'; // Added Dialog
 
 @NgModule({
-  declarations: [
-    EarningsComponent,
-    EarningsPageComponent
-  ],
+  declarations: [EarningsPageComponent],
   imports: [
-    CommonModule,
-    EarningsRoutingModule
-  ]
+    SharedModule, // Includes CommonModule, ReactiveFormsModule
+    EarningsRoutingModule,
+    // Material Modules for Earnings Page
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule, // Added
+    MatSnackBarModule, // Added
+    MatDialogModule, // Added
+  ],
 })
-export class EarningsModule { }
+export class EarningsModule {}
