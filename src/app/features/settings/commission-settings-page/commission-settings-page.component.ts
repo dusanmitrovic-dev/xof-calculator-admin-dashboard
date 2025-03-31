bce77e2abaa440741389bad7b2d9621ed6f3306e
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 17:53:26 +0200

    feat(settings): add CommissionSettingsPageComponent to settings module

---
+++ src/app/features/settings/commission-settings-page/commission-settings-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-commission-settings-page',
+  standalone: false,
+  templateUrl: './commission-settings-page.component.html',
+  styleUrl: './commission-settings-page.component.scss'
+})
+export class CommissionSettingsPageComponent {
+
+}
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
+import { CommissionSettingsPageComponent } from './commission-settings-page/commission-settings-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CommissionSettingsPageComponent
+  ],
   imports: [
     CommonModule,
     SettingsRoutingModule
