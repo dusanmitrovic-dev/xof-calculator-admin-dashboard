a9b4c30b873fdae6e701bffce3749f19a3842c9f
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:02:46 +0200

    feat(settings): add DisplaySettingsPageComponent to settings module

---
+++ src/app/features/settings/display-settings-page/display-settings-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display-settings-page',
+  standalone: false,
+  templateUrl: './display-settings-page.component.html',
+  styleUrl: './display-settings-page.component.scss'
+})
+export class DisplaySettingsPageComponent {
+
+}
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { CommissionSettingsPageComponent } from './commission-settings-page/commission-settings-page.component';
 import { BonusRulesPageComponent } from './bonus-rules-page/bonus-rules-page.component';
+import { DisplaySettingsPageComponent } from './display-settings-page/display-settings-page.component';
 
 
 @NgModule({
   declarations: [
     CommissionSettingsPageComponent,
-    BonusRulesPageComponent
+    BonusRulesPageComponent,
+    DisplaySettingsPageComponent
   ],
   imports: [
     CommonModule,
