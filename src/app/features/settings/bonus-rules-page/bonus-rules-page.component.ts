b15c564a78efcbb33c1fac8d4f4e5fb56c0028cd
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:01:12 +0200

    feat(settings): add BonusRulesPageComponent to settings module

---
+++ src/app/features/settings/bonus-rules-page/bonus-rules-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bonus-rules-page',
+  standalone: false,
+  templateUrl: './bonus-rules-page.component.html',
+  styleUrl: './bonus-rules-page.component.scss'
+})
+export class BonusRulesPageComponent {
+
+}
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { CommissionSettingsPageComponent } from './commission-settings-page/commission-settings-page.component';
+import { BonusRulesPageComponent } from './bonus-rules-page/bonus-rules-page.component';
 
 
 @NgModule({
   declarations: [
-    CommissionSettingsPageComponent
+    CommissionSettingsPageComponent,
+    BonusRulesPageComponent
   ],
   imports: [
     CommonModule,
