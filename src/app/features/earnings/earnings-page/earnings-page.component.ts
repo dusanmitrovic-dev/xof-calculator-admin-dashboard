70abdb2282ce652887ca6021604d6237c622712a
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:16:01 +0200

    feat(earnings): add EarningsPageComponent with template and styles

---
+++ src/app/features/earnings/earnings-page/earnings-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-earnings-page',
+  standalone: false,
+  templateUrl: './earnings-page.component.html',
+  styleUrl: './earnings-page.component.scss'
+})
+export class EarningsPageComponent {
+
+}
--- src/app/features/earnings/earnings.module.ts
+++ src/app/features/earnings/earnings.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { EarningsRoutingModule } from './earnings-routing.module';
 import { EarningsComponent } from './earnings.component';
+import { EarningsPageComponent } from './earnings-page/earnings-page.component';
 
 
 @NgModule({
   declarations: [
-    EarningsComponent
+    EarningsComponent,
+    EarningsPageComponent
   ],
   imports: [
     CommonModule,
