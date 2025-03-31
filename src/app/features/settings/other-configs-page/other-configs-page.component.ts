5e16c5689d7bee111fccfef5e40733bb43b76092
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-03-31 18:03:05 +0200

    feat(settings): add OtherConfigsPageComponent to settings module

---
+++ src/app/features/settings/other-configs-page/other-configs-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-other-configs-page',
+  standalone: false,
+  templateUrl: './other-configs-page.component.html',
+  styleUrl: './other-configs-page.component.scss'
+})
+export class OtherConfigsPageComponent {
+
+}
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -5,13 +5,15 @@ import { SettingsRoutingModule } from './settings-routing.module';
 import { CommissionSettingsPageComponent } from './commission-settings-page/commission-settings-page.component';
 import { BonusRulesPageComponent } from './bonus-rules-page/bonus-rules-page.component';
 import { DisplaySettingsPageComponent } from './display-settings-page/display-settings-page.component';
+import { OtherConfigsPageComponent } from './other-configs-page/other-configs-page.component';
 
 
 @NgModule({
   declarations: [
     CommissionSettingsPageComponent,
     BonusRulesPageComponent,
-    DisplaySettingsPageComponent
+    DisplaySettingsPageComponent,
+    OtherConfigsPageComponent
   ],
   imports: [
     CommonModule,
