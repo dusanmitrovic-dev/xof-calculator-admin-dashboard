3d0d6763e54136b6c28bf155631eaae44f5459d8
dusanmitrovic-dev <dusanmitrovicoffice@gmail.com>
2025-04-01 21:30:50 +0200

    feat(sidebar): refactor sidebar layout with toggle functionality and responsive design

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -5,6 +5,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-sidebar',
@@ -20,9 +21,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   ],
 })
 export class SidebarComponent {
-  isSidebarOpen = true;
+  isCollapsed = true;
 
   toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  logAndToggle(sidenav: MatSidenav) {
+    console.log('Toggle button clicked');
+    sidenav.toggle();
   }
 }
