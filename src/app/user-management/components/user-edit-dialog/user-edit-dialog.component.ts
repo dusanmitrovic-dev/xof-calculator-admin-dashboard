afc8253da3de548c5e97ba5083f8ae863bc9d5d5
dusanmitrovicoffice <dusanmitrovicoffice@gmail.com>
2025-04-16 01:25:46 +0000

    feat: generate user-edit-dialog component.

---
+++ src/app/user-management/components/user-edit-dialog/user-edit-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-edit-dialog',
+  imports: [],
+  templateUrl: './user-edit-dialog.component.html',
+  styleUrl: './user-edit-dialog.component.css'
+})
+export class UserEditDialogComponent {
+
+}
